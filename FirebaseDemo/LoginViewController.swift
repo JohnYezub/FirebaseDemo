0521bfa55be45a9831596d99e60efc4b58a55119
JohnYezub <buze@ya.ru>
2020-07-07 17:28:51 +0800

    Done basic setup with UI and Storyboard

---
+++ FirebaseDemo/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseDemo
+//
+//  Created by   admin on 07/07/20.
+//  Copyright © 2020 Evgeny Ezub. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
