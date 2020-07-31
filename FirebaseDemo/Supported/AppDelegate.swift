cd0468bfeac9a3fd0ec956ae1de0a1bd342e9c40
JohnYezub <buze@ya.ru>
2020-07-31 12:43:22 +0800

    add AppCenter

--- FirebaseDemo/Supported/AppDelegate.swift
+++ FirebaseDemo/Supported/AppDelegate.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 import Firebase
+
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+
+
 @UIApplicationMain
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        MSAppCenter.start("973c0f51-b13b-4766-8923-3c70865e2b17", withServices:[
+          MSAnalytics.self,
+          MSCrashes.self
+        ])
         FirebaseApp.configure()
         return true
     }
--- Podfile
+++ Podfile
@@ -10,5 +10,6 @@ target 'FirebaseDemo' do
 pod "Firebase/Core"
 pod "Firebase/Auth"
 pod "Firebase/Database"
+pod "AppCenter"
 
 end
